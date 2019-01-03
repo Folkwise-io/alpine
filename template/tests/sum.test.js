1599f495803a83ee1c929969cf481881f47ca3f7
András Varga <mail@andras.io>
2019-01-03 14:48:58 -0500

    Update `sum.test.js`

--- template/tests/sum.test.js
+++ template/tests/sum.test.js
@@ -1,9 +1,9 @@
 const assert = require('assert');
 
-module.exports = ({ sum }) => {
+module.exports = (methods) => {
   describe('sum', () => {
     it('should return 3 with parameters 1 and 2', () => {
-      assert.equal(sum(1, 2), 3);
+      assert.equal(methods.sum(1, 2), 3);
     });
   });
 };
