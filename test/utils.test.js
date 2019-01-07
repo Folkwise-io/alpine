a0edf782451f8a9900db780d3ba5b5b5288f5a3d
András Varga <mail@andras.io>
2019-01-07 10:27:53 -0500

    Format `utils.test.js`

--- test/utils.test.js
+++ test/utils.test.js
@@ -1,9 +1,14 @@
-const utils = require('../cli/utils');
 const { equal } = require('chai').assert;
+const utils = require('../cli/utils');
 
-describe('utils', function() {
-  it('can template', function() {
-    const output = utils.processTemplate('./resources/test_template.dot', {place: 'world'}, __dirname);
+describe('utils', () => {
+  it('can template', () => {
+    const output = utils.processTemplate(
+      './resources/test_template.dot',
+      { place: 'world' },
+      __dirname,
+    );
+
     equal(output, 'Hello world!');
-  })
+  });
 });
