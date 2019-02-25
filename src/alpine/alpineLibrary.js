898ec802b102f30e2de182be341b514fd9d00586
Andras Varga <mail@andras.io>
2019-02-25 14:59:54 -0500

    Integrate AlpineMiddleware into AlpineLibrary

--- src/alpine/alpineLibrary.js
+++ src/alpine/alpineLibrary.js
@@ -1,4 +1,5 @@
 import AlpineMethod from './method';
+import AlpineMiddleware from './middleware';
 import { messages } from '../config';
 
 const { DUPLICATE } = messages;
@@ -9,17 +10,31 @@ const AlpineLibrary = (config = {}) => {
   // Assign project configuration
   Object.assign(opts, config);
 
+  // Destructure important variables
+  const { methods, middleware } = opts;
+  const mwWrapper = AlpineMiddleware(middleware); // Used to wrap middleware on methods
+
   // Build the library
   const library = {};
-  Object.values(opts.methods).forEach((methodDefinition) => {
+  Object.values(methods).forEach((methodDefinition) => {
     if (library[methodDefinition.name]) {
       throw new Error(DUPLICATE(methodDefinition.name));
     }
 
-    library[methodDefinition.name] = AlpineMethod(methodDefinition);
+    library[methodDefinition.name] = mwWrapper.wrap(AlpineMethod(methodDefinition));
   });
 
-  return library;
+  // Alpine methods
+  const beforeAll = mw => mwWrapper.beforeAll(mw);
+  const afterAll = mw => mwWrapper.afterAll(mw);
+  const init = () => new Promise(resolve => resolve(library));
+
+  return {
+    ...library,
+    beforeAll: AlpineMethod({ cli: false, value: beforeAll }, true),
+    afterAll: AlpineMethod({ cli: false, value: afterAll }, true),
+    init: AlpineMethod({ cli: false, value: init }, true),
+  };
 };
 
 export default AlpineLibrary;
